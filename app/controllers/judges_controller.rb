5403416047aed6f7d6556e3aabffc68a5ff6ba7c
Kuroiwa Taku <kuroiwa.taku@gmail.com>
2017-12-09 07:32:42 +0000

    add ajax to good or bad button

--- app/controllers/judges_controller.rb
+++ app/controllers/judges_controller.rb
@@ -1,28 +1,26 @@
 class JudgesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
+    @type = params[:type]
     
-    if params[:type] == 'Good'
+    if @type == 'Good'
       current_user.good(@post)
-      flash[:success] = '投稿を Good! しました'
-    elsif params[:type] == 'Bad'
+    elsif @type == 'Bad'
       current_user.bad(@post)
-      flash[:success] = '投稿を Bad. しました'
     end
-    redirect_back(fallback_location: result_path)
+    # redirect_back(fallback_location: result_path)
   end
 
   def destroy
     @post = Post.find(params[:post_id])
+    @type = params[:type]
     
-    if params[:type] == 'Good'
+    if @type == 'Good'
       current_user.ungood(@post)
-      flash[:success] = '投稿の Good! を解除しました'
-    elsif params[:type] == 'Bad'
+    elsif @type == 'Bad'
       current_user.unbad(@post)
-      flash[:success] = '投稿の Bad. を解除しました'
     end
     
-    redirect_back(fallback_location: result_path)
+    # redirect_back(fallback_location: result_path)
   end
 end
